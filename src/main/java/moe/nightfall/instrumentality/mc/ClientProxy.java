1eeee4fcf73bca5edbd91847824d01e7302e5e3c
Victorious3 <vic@nightfall.moe>
2015-08-15 21:00:45 +0200

    Make miku turn, and disable lighting since it looks... weird.

--- src/main/java/moe/nightfall/instrumentality/mc/ClientProxy.java
+++ src/main/java/moe/nightfall/instrumentality/mc/ClientProxy.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import moe.nightfall.instrumentality.IMaterialBinder;
 import moe.nightfall.instrumentality.Main;
@@ -25,25 +26,37 @@ public class ClientProxy extends CommonProxy {
 		}
 	}
 	
+	private double interpolate(double last, double current, float partialTicks) {
+		return last + (current - last) * partialTicks;
+	}
+	
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onPlayerRender(RenderPlayerEvent.Pre event) {
 		
-        double x = event.entity.lastTickPosX + (event.entity.posX - event.entity.lastTickPosX) * event.partialRenderTick;
-        double y = event.entity.lastTickPosY + (event.entity.posY - event.entity.lastTickPosY) * event.partialRenderTick;
-        double z = event.entity.lastTickPosZ + (event.entity.posZ - event.entity.lastTickPosZ) * event.partialRenderTick;
+		EntityPlayer player = event.entityPlayer;
+		
+        double x = interpolate(player.lastTickPosX, player.posX, event.partialRenderTick);
+        double y = interpolate(player.lastTickPosY, player.posY, event.partialRenderTick);
+        double z = interpolate(player.lastTickPosZ, player.posZ, event.partialRenderTick);
+        
+        double pitch   = interpolate(player.prevRotationPitch  , player.rotationPitch  , event.partialRenderTick);
+        double yaw     = interpolate(player.prevRotationYaw    , player.rotationYaw    , event.partialRenderTick);
+        double yawHead = interpolate(player.prevRotationYawHead, player.rotationYawHead, event.partialRenderTick);
+        double rotBody = interpolate(player.prevRenderYawOffset, player.renderYawOffset, event.partialRenderTick);
         
         x -= RenderManager.renderPosX;
         y -= RenderManager.renderPosY;
         z -= RenderManager.renderPosZ;
         
         PMXModel model = Main.pm[0];
-        float scale = 1F / (model.height / event.entityPlayer.height);
+        float scale = 1F / (model.height / player.height);
         
         GL11.glPushMatrix();
-        GL11.glTranslated(x, y - event.entityPlayer.height + event.entityPlayer.eyeHeight, z);
+        GL11.glRotated(180 - rotBody, 0, 1, 0);
+        GL11.glTranslated(x, y - player.height + player.eyeHeight, z);
         GL11.glScalef(scale, scale, scale);
         GL11.glDisable(GL11.GL_CULL_FACE);
-        GL11.glShadeModel(GL11.GL_SMOOTH);
+        GL11.glDisable(GL11.GL_LIGHTING);
         
         model.render(new IMaterialBinder() {
         	@Override
@@ -53,7 +66,7 @@ public class ClientProxy extends CommonProxy {
     	}, Main.shaderBoneTransform);
         
         GL11.glEnable(GL11.GL_CULL_FACE);
-        GL11.glShadeModel(GL11.GL_FLAT);
+        GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glPopMatrix();
         
         event.setCanceled(true);
