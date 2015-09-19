5fb3bf1bc0d8c50d45ecd26249a6010dcf184eb3
Victorious3 <vic@nightfall.moe>
2015-09-19 13:16:41 +0200

    EditorHostGui

--- src/main/scala/moe/nightfall/instrumentality/mc/gui/EditorHostGui.scala
+++ src/main/scala/moe/nightfall/instrumentality/mc/gui/EditorHostGui.scala
@@ -26,60 +26,49 @@ import java.io.IOException;
 /**
  * Created on 18/08/15.
  */
-public class EditorHostGui extends GuiScreen {
+class EditorHostGui extends GuiScreen {
 
-    public EditElement hostedElement;
+    var hostedElement : EditElement = null
 
-    public EditorHostGui() {
-    }
-
-    @Override
-    public void initGui() {
+    override def initGui() {
         if (UIFont.fontDB == null)
-            try {
-                UIFont.setFont(Minecraft.getMinecraft().mcDefaultResourcePack.getInputStream(new ResourceLocation("instrumentality:/font.txt")));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            UIFont.setFont(Minecraft.getMinecraft.mcDefaultResourcePack.getInputStream(new ResourceLocation("instrumentality:/font.txt")))
         if (hostedElement == null)
-            changePanel(UIUtils.createGui());
+            changePanel(UIUtils.createGui())
     }
 
-    @Override
-    public void setWorldAndResolution(Minecraft p_146280_1_, int p_146280_2_, int p_146280_3_) {
-        super.setWorldAndResolution(p_146280_1_, p_146280_2_, p_146280_3_);
+    override def setWorldAndResolution(mc : Minecraft, width : Int, height : Int) {
+        super.setWorldAndResolution(mc, width, height)
         if (hostedElement != null)
-            hostedElement.setSize(Display.getWidth(), Display.getHeight());
+            hostedElement.setSize(Display.getWidth(), Display.getHeight())
     }
 
-    @Override
-    public void drawScreen(int par1, int par2, float par3) {
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0, Display.getWidth(), Display.getHeight(), 0, 0, 1024);
-        GL11.glPushMatrix();
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glPushMatrix();
-        GL11.glLoadIdentity();
-        hostedElement.draw(Display.getWidth(), Display.getHeight());
-        GL11.glPopMatrix();
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glPopMatrix();
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
+    override def drawScreen(xCoord : Int, yCoord : Int, partialTick : Float) {
+        GL11.glDisable(GL11.GL_TEXTURE_2D)
+        GL11.glMatrixMode(GL11.GL_PROJECTION)
+        GL11.glLoadIdentity()
+        GL11.glOrtho(0, Display.getWidth(), Display.getHeight(), 0, 0, 1024)
+        GL11.glPushMatrix()
+        GL11.glMatrixMode(GL11.GL_MODELVIEW)
+        GL11.glPushMatrix()
+        GL11.glLoadIdentity()
+        hostedElement.draw(Display.getWidth(), Display.getHeight())
+        GL11.glPopMatrix()
+        GL11.glMatrixMode(GL11.GL_PROJECTION)
+        GL11.glPopMatrix()
+        GL11.glMatrixMode(GL11.GL_MODELVIEW)
+        GL11.glEnable(GL11.GL_TEXTURE_2D)
     }
 
-    @Override
-    public void updateScreen() {
-        UIUtils.update(hostedElement);
-        hostedElement.update(0.05d);
+    override def updateScreen() {
+        UIUtils.update(hostedElement)
+        hostedElement.update(0.05d)
     }
 
-    public void changePanel(EditElement newPanel) {
+    def changePanel(newPanel : EditElement) {
         if (hostedElement != null)
-            hostedElement.cleanup();
-        hostedElement = newPanel;
-        hostedElement.setSize(width, height);
+            hostedElement.cleanup()
+        hostedElement = newPanel
+        hostedElement.setSize(width, height)
     }
 }
